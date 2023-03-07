a885c9aee0163400be25ffc842efc3905a928c97
= <=>
2023-03-08 00:54:56 +0700

    fix delete

--- main.cpp
+++ main.cpp
@@ -8,7 +8,11 @@ int School::getNumsClass() {
 }
 
 void School::viewClass() {
-
+	Class* phead = head;
+	while(phead){
+		cout << "ID: " << phead->id << "     ,type: " << phead->type << "      ,status: " << phead->is_Set << "\n";
+		phead = phead->next;
+	}
 }
 
 bool School::is_Empty() {
@@ -16,7 +20,7 @@ bool School::is_Empty() {
 }
 
 bool School::addClass(string id, typeClass type, bool is_Set, int index) {
-	if (index < 0 || index >= this->getNumsClass()) {
+	if (index < 0 || index > this->getNumsClass()) {
 		return 0; // bao loi cho user
 	}
 	else {
@@ -30,13 +34,13 @@ bool School::addClass(string id, typeClass type, bool is_Set, int index) {
 				newClass->next = head;
 				head = newClass;
 			}
-			else if (index == this->numsClass - 1) {
+			else if (index == this->numsClass) {
 				tail->next = newClass;
 				tail = newClass;
 			}
 			else {
 				Class* phead = head;
-				for (int i = 0; i < index - 1; i++) {
+				for (int i = 0; i < index - 1 ; i++) {
 					phead = phead->next;
 				}
 				newClass->next = phead->next;
@@ -50,13 +54,84 @@ bool School::addClass(string id, typeClass type, bool is_Set, int index) {
 
 bool School::addClass(string id, typeClass type, bool is_Set) {
 	Class* newClass = new Class(id, type, is_Set);
-	tail->next = newClass;
-	tail = newClass;
+	if (this->numsClass == 0) {
+		head = newClass;
+		tail = head;
+	}
+	else {
+		tail->next = newClass;
+		tail = newClass;
+	}
+	this->numsClass++;
 	return 1;
+}
+
+int School::findIndex(string id) {
+	Class* phead = head;
+	for (int i = 0; i < this->numsClass; i++) {
+		if (phead->id == id) return i;
+		phead = phead->next;
+	}
+	return -1;
+}
+
+School::Class *School::findClass(string id) {
+	Class* phead = head;
+	while (phead) if (phead->id == id) return phead;
+	return NULL;
+}
+
+bool School::deleteClass(string id) {
+	int index = this->findIndex(id);
+	if (index < 0) return 0;
+	else {
+		Class* deleteClass = head;
+		if (index == 0) {
+			head = deleteClass->next;
+			delete(deleteClass);
+		}
+		//else if (index == this->numsClass - 1) {
+		//	for (int i = 0; i < this->numsClass - 2; i++) deleteClass = deleteClass->next;
+		//	tail = deleteClass;
+		//	delete(deleteClass->next);
+		//}
+		//else {
+		//	for (int i = 0; i < index - 2; i++) deleteClass = deleteClass->next;
+		//	deleteClass->next = deleteClass->next->next;
+		//	delete(deleteClass->next);
+		//}
+		this->numsClass--;
+		return 1;
+	}
+}
+
+
+void School::setClass(string id, typeClass type, bool is_Set) {
+	Class *classFound = findClass(id);
+	classFound->setClassSet(is_Set);
+	classFound->setTypeClass(type);
 }
 
 int main() {
 	School* mySchool = new School(0);
+	int nums;
+	cout << "input nums: ";
+	cin >> nums;
+	string id;
+	int typeInput;
+	bool is_Set;
+	for (int i = 0; i < nums; i++) {
+		cout << "input id: ";
+		cin >> id;
+		cout << "input type (TH: 0,LT: 1): ";
+		cin >> typeInput;
+		typeClass type = static_cast<typeClass>(typeInput);
+		mySchool->addClass(id,type,0);
+	}
+	mySchool->viewClass();
+	string test = mySchool->deleteClass("0") ? "xoa roi" : "chua xoa";
+	cout << test << endl;
+	mySchool->viewClass();
 	return 0;
 }
 
--- main.h
+++ main.h
@@ -15,15 +15,16 @@ private:
 	Class* tail;
 	int numsClass;
 public:
-	School(int numsClass) :numsClass(numsClass) {}
+	School(int numsClass) : head(NULL), tail(NULL), numsClass(numsClass) {}
 	int getNumsClass();
 	bool is_Empty();
 	void viewClass();
 	bool addClass(string, typeClass, bool, int);
 	bool addClass(string, typeClass, bool);
-	void deleteClass();
-	void setClass();
-	void searchClass(string);
+	int findIndex(string);
+	Class* findClass(string);
+	bool deleteClass(string);
+	void setClass(string,typeClass,bool);
 
 public:
 	class Class {
@@ -35,5 +36,14 @@ public:
 		friend class School;
 	public:
 		Class(string id, typeClass type, bool is_Set) : id(id), type(type), is_Set(is_Set), next(NULL) {}
+		void setClassSet(bool is_Set) {
+			this->is_Set = is_Set;
+		}
+		void setTypeClass(typeClass type) {
+			this->type = type;
+		}
+		void setIDClass(string id) {
+			this->id = id;
+		}
 	};
 };
